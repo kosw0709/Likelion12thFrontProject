8b43fd312a39fb534bb8e08d208b4dbac650c991
Martin <kosw0709@naver.com>
2024-01-18 15:10:13 +0900

    upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import LikelionHeader from './LikelionHeader';
+import TrackInfo from './TrackInfo';
+import Activity from './Activity';
+import FAQ from './FAQ';
+
+function App() {
+  return (
+    <div>
+    <LikelionHeader></LikelionHeader>
+    <TrackInfo></TrackInfo>
+    <Activity></Activity>
+    <FAQ></FAQ>
+    </div>
+  );
+}
+
+export default App;
